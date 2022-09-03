e99e9873cfb399acff4f3a643418d63cbc0607f8
DarkLord76865 <40371578+darklord76865@users.noreply.github.com>
2022-09-03 21:51:03 +0200

    finish GUI

--- main.py
+++ main.py
@@ -23,12 +23,15 @@ def key_release(event):
 	global key_pressed
 	key_pressed = False
 
-def hover_thickness(event, name, widget, thickness):
+def hover_difficulty(event, name, widget, thickness):
 	global selected_gamemode
 	if selected_gamemode != name:
 		widget.config(highlightthickness=thickness)
 
-def difficulty_change(event, clicked_mode, widgets, widget_ind):
+def hover_restart(event, widget, img_ind, images):
+	widget.config(image=images[img_ind])
+
+def click_difficulty(event, clicked_mode, widgets, widget_ind):
 	global selected_gamemode
 	if clicked_mode != selected_gamemode:
 		selected_gamemode = clicked_mode
@@ -37,11 +40,14 @@ def difficulty_change(event, clicked_mode, widgets, widget_ind):
 				widgets[i].config(highlightthickness=5)
 			else:
 				widgets[i].config(highlightthickness=2)
-		print(selected_gamemode)
 		new_game(None, selected_gamemode)
 
+def click_restart(event):
+	global selected_gamemode
+	new_game(None, selected_gamemode)
+
 def new_game(event, difficulty):
-	global digits_canvas
+	global digits_canvas, digits
 	digits_canvas.delete("all")
 
 	match difficulty:
@@ -53,27 +59,30 @@ def new_game(event, difficulty):
 			num_len = 8
 
 	digits_coords = []
-
-	number = randint(10 ** (num_len - 1), int("".join(map(str, [9 for i in range(num_len)]))))
+	number = randint(10 ** (num_len - 1), int("".join(map(str, [9 for _ in range(num_len)]))))
 	number_digits = [int(x) for x in str(number)]
 	len_number = len(number_digits)
-	start_coord = (500 - (len_number * 30 + (len_number - 1) * 11)) // 2
+	start_coord = (500 - (len_number * 35 + (len_number - 1) * 15)) // 2
 	line_or_space = True
 	for i in range(2 * len_number - 1):
 		if line_or_space:
-			digits_canvas.create_line(start_coord, 40, start_coord + 30, 40, fill="white", width=4)
-			digits_coords.append(start_coord + 14)
+			digits_canvas.create_line(start_coord, 50, start_coord + 35, 50, fill="white", width=4)
+			digits_coords.append(start_coord + 17)
 			line_or_space = False
-			start_coord += 30
+			start_coord += 35
 		else:
-			start_coord += 11
+			start_coord += 15
 			line_or_space = True
 
-def GUI():
+	digits = []
+	for i in range(len(digits_coords)):
+		digits.append([None, number_digits[i], digits_canvas.create_text(digits_coords[i], 55, text="*", font=("Helvetica", 40, "bold"), fill="white", activefill="white", anchor="s")])
+
+def gui():
 	global digits_canvas, selected_gamemode
 
 	root = Tk()
-	root.geometry(f'500x500+{root.winfo_screenwidth() // 2 - 250}+{root.winfo_screenheight() // 2 - 250}')
+	root.geometry(f'500x330+{root.winfo_screenwidth() // 2 - 250}+{root.winfo_screenheight() // 2 - 165}')
 	root.resizable(False, False)
 	root.title('NumGuessr')
 	root.iconbitmap(resource_path("icon.ico"))
@@ -89,57 +98,104 @@ def GUI():
 	title.place(x=0, y=0, width=500, height=85)
 
 	difficulty_easy = Label(text="Easy", font=("Helvetica", 18, "bold"),
-	                        background="#9ECFC2", activebackground="#9ECFC2",
+	                        background="#6E9087", activebackground="#6E9087",
 	                        foreground="white", activeforeground="white",
 	                        highlightthickness=5, highlightcolor="white", highlightbackground="white",
 	                        borderwidth=0)
-	difficulty_easy.place(x=65, y=85, width=120, height=40)
-	difficulty_easy.bind("<Enter>", lambda event: hover_thickness(event, "easy", difficulty_easy, 5))
-	difficulty_easy.bind("<Leave>", lambda event: hover_thickness(event, "easy", difficulty_easy, 2))
+	difficulty_easy.place(x=65, y=278, width=120, height=40)
+	difficulty_easy.bind("<Enter>", lambda event: hover_difficulty(event, "easy", difficulty_easy, 5))
+	difficulty_easy.bind("<Leave>", lambda event: hover_difficulty(event, "easy", difficulty_easy, 2))
 	difficulty_medium = Label(text="Medium", font=("Helvetica", 18, "bold"),
-	                          background="#9ECFC2", activebackground="#9ECFC2",
+	                          background="#6E9087", activebackground="#6E9087",
 	                          foreground="white", activeforeground="white",
 	                          highlightthickness=2, highlightcolor="white", highlightbackground="white",
 	                          borderwidth=0)
-	difficulty_medium.place(x=190, y=85, width=120, height=40)
-	difficulty_medium.bind("<Enter>", lambda event: hover_thickness(event, "medium", difficulty_medium, 5))
-	difficulty_medium.bind("<Leave>", lambda event: hover_thickness(event, "medium", difficulty_medium, 2))
+	difficulty_medium.place(x=190, y=278, width=120, height=40)
+	difficulty_medium.bind("<Enter>", lambda event: hover_difficulty(event, "medium", difficulty_medium, 5))
+	difficulty_medium.bind("<Leave>", lambda event: hover_difficulty(event, "medium", difficulty_medium, 2))
 	difficulty_hard = Label(text="Hard", font=("Helvetica", 18, "bold"),
-	                        background="#9ECFC2", activebackground="#9ECFC2",
+	                        background="#6E9087", activebackground="#6E9087",
 	                        foreground="white", activeforeground="white",
 	                        highlightthickness=2, highlightcolor="white", highlightbackground="white",
 	                        borderwidth=0)
-	difficulty_hard.place(x=315, y=85, width=120, height=40)
-	difficulty_hard.bind("<Enter>", lambda event: hover_thickness(event, "hard", difficulty_hard, 5))
-	difficulty_hard.bind("<Leave>", lambda event: hover_thickness(event, "hard", difficulty_hard, 2))
-
-	difficulty_easy.bind("<Button-1>", lambda event: difficulty_change(event, "easy", [difficulty_easy, difficulty_medium, difficulty_hard], 0))
-	difficulty_medium.bind("<Button-1>", lambda event: difficulty_change(event, "medium", [difficulty_easy, difficulty_medium, difficulty_hard], 1))
-	difficulty_hard.bind("<Button-1>", lambda event: difficulty_change(event, "hard", [difficulty_easy, difficulty_medium, difficulty_hard], 2))
+	difficulty_hard.place(x=315, y=278, width=120, height=40)
+	difficulty_hard.bind("<Enter>", lambda event: hover_difficulty(event, "hard", difficulty_hard, 5))
+	difficulty_hard.bind("<Leave>", lambda event: hover_difficulty(event, "hard", difficulty_hard, 2))
+	difficulty_easy.bind("<Button-1>", lambda event: click_difficulty(event, "easy", (difficulty_easy, difficulty_medium, difficulty_hard), 0))
+	difficulty_medium.bind("<Button-1>", lambda event: click_difficulty(event, "medium", (difficulty_easy, difficulty_medium, difficulty_hard), 1))
+	difficulty_hard.bind("<Button-1>", lambda event: click_difficulty(event, "hard", (difficulty_easy, difficulty_medium, difficulty_hard), 2))
 
 	digits_canvas = Canvas(root,
-	                       background="blue", #"#9ECFC2",
+	                       background="#9ECFC2",
 	                       highlightthickness=0, borderwidth=0)
-	digits_canvas.place(x=0, y=250, height=50, width=500)
+	digits_canvas.place(x=0, y=100, height=60, width=500)
 
 	restart_img_small = PhotoImage(file=resource_path("restart-small.png"))
 	restart_img_big = PhotoImage(file=resource_path("restart-big.png"))
-
 	restart = Label(image=restart_img_small,
 	                background="#9ECFC2", activebackground="#9ECFC2",
 	                borderwidth=0, highlightthickness=0)
 	restart.place(x=0, y=0, height=45, width=45)
+	restart.bind("<Enter>", lambda event: hover_restart(event, restart, 1, (restart_img_small, restart_img_big)))
+	restart.bind("<Leave>", lambda event: hover_restart(event, restart, 0, (restart_img_small, restart_img_big)))
+	restart.bind("<Button-1>", lambda event: click_restart(event))
+
+	correct_digits = Label(text="Correct digits: ", font=("Helvetica", 18, "bold"),
+	                       background="#9ECFC2", activebackground="#9ECFC2",
+	                       foreground="white", activeforeground="white",
+	                       borderwidth=0, highlightthickness=0,
+	                       anchor="e")
+	correct_digits.place(x=0, y=210, width=195, height=30)
+	correct_places = Label(text="Correct places: ", font=("Helvetica", 18, "bold"),
+	                       background="#9ECFC2", activebackground="#9ECFC2",
+	                       foreground="white", activeforeground="white",
+	                       borderwidth=0, highlightthickness=0,
+	                       anchor="e")
+	correct_places.place(x=0, y=240, width=195, height=30)
+	tries = Label(text="Tries: ", font=("Helvetica", 18, "bold"),
+	              background="#9ECFC2", activebackground="#9ECFC2",
+	              foreground="white", activeforeground="white",
+	              borderwidth=0, highlightthickness=0,
+	              anchor="e")
+	tries.place(x=0, y=180, width=195, height=30)
+
+	correct_digits_num = Label(text="0", font=("Helvetica", 18, "bold"),
+	                           background="#9ECFC2", activebackground="#9ECFC2",
+	                           foreground="white", activeforeground="white",
+	                           borderwidth=0, highlightthickness=0,
+	                           anchor="w")
+	correct_digits_num.place(x=200, y=210, width=300, height=30)
+	correct_places_num = Label(text="0", font=("Helvetica", 18, "bold"),
+	                           background="#9ECFC2", activebackground="#9ECFC2",
+	                           foreground="white", activeforeground="white",
+	                           borderwidth=0, highlightthickness=0,
+	                           anchor="w")
+	correct_places_num.place(x=200, y=240, width=300, height=30)
+	tries_num = Label(text="0", font=("Helvetica", 18, "bold"),
+	                  background="#9ECFC2", activebackground="#9ECFC2",
+	                  foreground="white", activeforeground="white",
+	                  borderwidth=0, highlightthickness=0,
+	                  anchor="w")
+	tries_num.place(x=200, y=180, width=300, height=30)
+
+	end_status = Label(text="", font=("Helvetica", 18, "bold"),
+	                   background="#9ECFC2", activebackground="#9ECFC2",
+	                   foreground="white", activeforeground="white",
+	                   borderwidth=0, highlightthickness=0,
+	                   anchor=CENTER)
+	end_status.place(y=180, height=90, width=225, x=275)
 
 	new_game(0, selected_gamemode)
 
 	root.mainloop()
 
 def main():
-	global key_pressed, selected_gamemode
+	global key_pressed, selected_gamemode, digits
 	key_pressed = False
 	selected_gamemode = "easy"
+	digits = []
 
-	GUI()
+	gui()
 
 
 if __name__ == '__main__':
